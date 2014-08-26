798a0f6d67818b4f8911254c2813bf85058d4028
tolis-e <tolisemm@yahoo.gr>
2014-08-26 14:54:25 +0300

    remove unnecessary method

--- src/test/java/org/arquillian/wildfly/example/Greeter.java
+++ src/test/java/org/arquillian/wildfly/example/Greeter.java
@@ -1,12 +1,7 @@
 package org.arquillian.wildfly.example;
 
-import java.io.PrintStream;
 
 public class Greeter {
-
-    public void greet(PrintStream to, String name) {
-        to.println(createGreeting(name));
-    }
 
     public String createGreeting(String name) {
         return "Hello, " + name + "!";
